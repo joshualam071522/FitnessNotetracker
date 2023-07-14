2b279d27ad4bc50681fb0a22f3303ecedcd84336
joshualam071522 <joshualam071522@gmail.com>
2023-07-13 19:46:18 -0700

    added initial user routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
+const userRoutes = require('./user-routes.js');
 const mealRoutes = require('./meal-routes.js');
-const workoutRoutes = require('./workout-routes.js')
+const workoutRoutes = require('./workout-routes.js');
 
+router.use('/user', userRoutes);
 router.use('/meal', mealRoutes);
 router.use('/workout', workoutRoutes);
 
