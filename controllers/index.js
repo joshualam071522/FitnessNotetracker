ca60d5c0b605016cad9f232c0ceb661cdafbc404
joshualam071522 <joshualam071522@gmail.com>
2023-07-19 04:58:29 -0700

    reformatted some handlebars files to add functionality and added js files to organize clientside functions

--- controllers/index.js
+++ controllers/index.js
@@ -5,11 +5,13 @@ const homeRoutes = require('./home-routes')
 const loginRoutes = require('./login-routes');
 const signupRoutes = require('./signup-routes');
 const fitnessRoutes = require('./fitness-routes');
+const mealRoutes = require('./meal-routes');
 
 router.use('/', homeRoutes);
 router.use('/login', loginRoutes);
 router.use('/signup', signupRoutes);
 router.use('/fitness', fitnessRoutes);
+router.use('/meal', mealRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
