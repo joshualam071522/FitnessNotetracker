a910c7df2147ef51915842ad77cd5da3882fa7fb
joshualam071522 <joshualam071522@gmail.com>
2023-07-19 20:01:36 -0700

    added preliminary code for req.sessions.logged in variable, added functionality for workout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
 
 //TODO add handlebars template that renders homepage
-// router.get('/', async (req, res) => {
+router.get('/', async (req, res) => {
     
-//     res.render('homepage');
-//   });
+    res.render('homepage', {loggedIn: req.session.loggedIn});
+  });
+
+router.post("/logout", (req, res) => {
+    req.session.destroy(() => {
+      res.redirect("/login");
+    });
+  });
 
 module.exports = router;
